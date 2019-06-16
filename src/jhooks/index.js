a00d2c8d34b501e4092cc89bf3702e15d8041da4
eunchu <lovely-ej01@nate.com>
2019-06-16 22:48:01 +0900

    jhooks 추가

---
+++ src/jhooks/index.js
@@ -0,0 +1,1 @@
+export { useClick, useFadeIn, useHover, useTabs } from "./jhooks";
---
+++ src/jhooks/jhooks.js
@@ -0,0 +1,80 @@
+import { useState, useEffect, useRef } from "react";
+
+/**
+ *
+ * @param {Number} initialTab
+ * @param {Array} allTabs
+ */
+export const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) return;
+
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
+
+/**
+ *
+ * @param {Function} onClick
+ */
+export const useClick = onClick => {
+  if (typeof onClick !== "function") return;
+
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, [onClick]);
+
+  return element;
+};
+
+/**
+ *
+ * @param {Number} duration
+ * @param {Number} delay
+ */
+export const useFadeIn = (duration = 1, delay = 0) => {
+  if (typeof duration !== "number" || typeof delay !== "number") return;
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, [delay, duration]);
+  return { ref: element, style: { opacity: 0 } };
+};
+
+/**
+ *
+ * @param {Function} onHover
+ */
+export const useHover = onHover => {
+  if (typeof onHover !== "function") return;
+
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("mouseenter", onHover);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("mouseenter", onHover);
+      }
+    };
+  }, [onHover]);
+
+  return element;
+};
